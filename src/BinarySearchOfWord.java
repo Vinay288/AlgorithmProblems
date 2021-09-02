c78cd8c70a2fcfcbc424a0d9167d5831b206182d
Vinay288 <vinayhiremath288@gmail.com>
2021-09-02 23:50:46 +0530

    REFACTOR:implemented binarysearch using generics

--- src/BinarySearchOfWord.java
+++ src/BinarySearchOfWord.java
@@ -2,27 +2,34 @@ import java.util.Arrays;
 
 public class BinarySearchOfWord {
 	public static void main(String[] args) {
-		String array[] = { "ABCD", "AAD", "ABW", "BB" };
-		String search = "BB";
-		Arrays.sort(array);
-		int searchIndex = binarySearch(array, search);
+		String stringArray[] = { "ABCD", "AAD", "ABW", "BB" };
+		String searchKey = "BB";
+		Arrays.sort(stringArray);
+		int searchIndex = binarySearch(stringArray, searchKey);
+		if (searchIndex == -1)
+			System.out.println("not found");
+		else
+			System.out.println("found at position " + searchIndex);
+		Integer[] integerArray = { 1, 2, 3, 4, 5, 6, 7, 8 };
+		Integer search = 5;
+		searchIndex = binarySearch(integerArray, search);
 		if (searchIndex == -1)
 			System.out.println("not found");
 		else
 			System.out.println("found at position " + searchIndex);
 	}
 
-	public static int binarySearch(String[] stringArray, String search) {
+	public static <T extends Comparable<T>> int binarySearch(T[] inputArray, T searchKey) {
 		int low = 0;
-		int high = stringArray.length - 1;
+		int high = inputArray.length - 1;
 		int mid;
 
 		while (low <= high) {
 			mid = (low + high) / 2;
 
-			if (stringArray[mid].compareTo(search) < 0) {
+			if (inputArray[mid].compareTo(searchKey) < 0) {
 				low = mid + 1;
-			} else if (stringArray[mid].compareTo(search) > 0) {
+			} else if (inputArray[mid].compareTo(searchKey) > 0) {
 				high = mid - 1;
 			} else {
 				return mid;
