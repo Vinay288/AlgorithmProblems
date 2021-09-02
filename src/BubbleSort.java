481949e5ac8ca68b1872c8244d9d3b40d67ec27a
Vinay288 <vinayhiremath288@gmail.com>
2021-09-02 23:51:05 +0530

    REFACTOR:implemented bubble sort using generics

--- src/BubbleSort.java
+++ src/BubbleSort.java
@@ -2,15 +2,21 @@
 public class BubbleSort {
 
 	public static void main(String[] args) {
-		int[] inputArray = { 20, 9, 7, 101, 23, 44, 12, 78, 34, 32 };
+		Integer[] inputArray = { 20, 9, 7, 101, 23, 44, 12, 78, 34, 32 };
 		System.out.println("before sort");
 		for (int i : inputArray)
 			System.out.print(i + " ");
 		System.out.println("");
-		for (int i = 0; i < inputArray.length-1; i++) {
-			for (int j = 0; j < inputArray.length; j++) {
-				if (inputArray[i] < inputArray[j]) {
-					int temp = inputArray[i];
+		bubbleSort(inputArray);
+		String[] stringArray = { "AA", "BB", "CC" };
+		bubbleSort(stringArray);
+	}
+
+	static <T extends Comparable<T>> void bubbleSort(T[] inputArray) {
+		for (int i = 0; i < inputArray.length - 1; i++) {
+			for (int j = i; j < inputArray.length; j++) {
+				if (inputArray[i].compareTo(inputArray[j]) > 0) {
+					T temp = inputArray[i];
 					inputArray[i] = inputArray[j];
 					inputArray[j] = temp;
 				}
